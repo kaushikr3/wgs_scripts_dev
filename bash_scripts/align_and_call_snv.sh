c503ce65e13e8c2484260fa6065cb373bb140ffb
Natalie Thornton <nat4004@node158.panda>
2021-03-22 12:11:38 -0400

    adjustments to get running on cluster

--- bash_scripts/align_and_call_snv.sh
+++ bash_scripts/align_and_call_snv.sh
@@ -15,8 +15,8 @@
 # usage:
 function usage {
         echo "Usage: $(basename $0) [-ref] [-in]" 2>&1
-        echo '   -ref   path to reference_genome.fa'
-        echo '   -in   path to directory holding UNMERGED fastq'
+        echo '   -R   path to reference_genome.fa'
+        echo '   -I   path to directory holding UNMERGED fastq'
         exit 1
 }
 
@@ -25,12 +25,12 @@ if [[ ${#} -eq 0 ]]; then
 fi
 
 # Define list of arguments expected in the input
-optstring="ref:in:"
+optstring="R:I:"
 
 while getopts ${optstring} arg; do
   case "${arg}" in
-    ref) REF=${OPTARG} ;;
-    in) UNMERGED_DIR=${OPTARG} ;;
+    R) REF="${OPTARG}" ;;
+    I) UNMERGED_DIR="${OPTARG}" ;;
 
     ?)
       echo "Invalid option: -${OPTARG}."
@@ -39,10 +39,6 @@ while getopts ${optstring} arg; do
       ;;
   esac
 done
-
-echo " ----  Processing fastq.gz files from ${UNMERGED_DIR}  ----" >> reports/"${UNMERGED_DIR}.log"
-echo " ----  Aligning files against ref: ${REF} ----" >> reports/"${UNMERGED_DIR}.log"
-
 
 # DIRECTORY SETUP
 mkdir fastq
@@ -57,19 +53,27 @@ mkdir gatk
 mkdir freebayes
 
 
+echo " ----  Processing fastq.gz files from ${UNMERGED_DIR}  ----"
+echo " ----  Aligning files against ref: ${REF} ----"
+
+
 # MERGE LANE FASTQS
 for f in ${UNMERGED_DIR}/*L001_R1_001.fastq.gz
-do
-  [[ -e "$f" ]] || break  # handle the case of no *L001_R1_001.fastq.gz files
+do 
+		#[[ -e "$f" ]] || break  # handle the case of no *L001_R1_001.fastq.gz files
+	
+		echo "$f"
+  		BASE="${f/L001_R1_001.fastq.gz/}"
+		OUTNAME=$(basename ${BASE})
 
-  BASE="${f/L001_R1_001.fastq.gz/}"
-  FILES_R1=(${BASE}*R1_001.fastq.gz)
-  cat "${FILES_R1[@]}" > fastq/merged_untrimmed/"${BASE}R1_001.fastq.gz"
-  echo "Merged "${FILES_R1}" into ${BASE}_R1_001.fastq.gz" >> reports/"${UNMERGED_DIR}.log"
+  		FILES_R1=(${BASE}*R1_001.fastq.gz)
+  		FILES_R2=(${BASE}*R2_001.fastq.gz)
+		
+  		cat "${FILES_R1[@]}" > fastq/merged_untrimmed/"${OUTNAME}R1_001.fastq.gz"
+  		echo "Merged "${FILES_R1}" into ${OUTNAME}_R1_001.fastq.gz"
 
-  FILES_R2=(${BASE}*R2_001.fastq.gz)
-  cat "${FILES_R2[@]}" > fastq/merged_untrimmed/"${BASE}R2_001.fastq.gz"
-  echo "Merged "${FILES_R2}" into ${BASE}_R2_001.fastq.gz" >> reports/"${UNMERGED_DIR}.log"
+  		cat "${FILES_R2[@]}" > fastq/merged_untrimmed/"${OUTNAME}R2_001.fastq.gz"
+  		echo "Merged "${FILES_R2}" into ${OUTNAME}_R2_001.fastq.gz"
 
 done
 
@@ -78,7 +82,7 @@ done
 for f in fastq/merged_untrimmed/*_001.fastq.gz
 
 	do
- 			echo Running fastqc on "$f" >> reports/"${UNMERGED_DIR}.log"
+ 			echo Running fastqc on "$f"
 			fastqc -t 2 --extract -o reports/fastqc_untrimmed_out "$f"
 	done
 
@@ -86,15 +90,11 @@ for f in fastq/merged_untrimmed/*_001.fastq.gz
 # TRIM AND GENERATE TRIMMED FASTQC
 for f in fastq/merged_untrimmed/*R1_001.fastq.gz
 	do
-			echo "Running TrimGalore on:  ${a}  ${a/R1/R2}" >> reports/"${UNMERGED_DIR}.log"
-			/Users/nataliethornton/biotools/TrimGalore-0.6.5/trim_galore \
-					--path_to_cutadapt /Users/nataliethornton/anaconda3/envs/wgs/bin/cutadapt \
-					--quality 30 \
-					--paired \
-					--fastqc \
-					--output_dir fastq \
-					--fastqc_args "-t 2 --extract -o reports/fastqc_trimmed_out" \
+			echo "Running TrimGalore on:  ${f}  ${f/R1/R2}"
+			trim_galore --quality 30 --paired --output_dir fastq \
+					--fastqc --fastqc_args "-t 2 --extract -o reports/fastqc_trimmed_out" \
 					"${f}" "${f/R1/R2}"
+				#	--path_to_cutadapt /Users/nataliethornton/anaconda3/envs/wgs/bin/cutadapt \
 	done
 
 
@@ -103,10 +103,12 @@ for f in fastq/*R1_001_val_1.fq.gz
 	do
 			# Use MC2155 for WGS of Msmeg
 			# Use H37RvCO4 for WGS of H37Rv Mtb to get closest reference genome
-			echo "Aligning ${f} and ${f/R1_001_val_1/R2_001_val_2} with BWA MEM" >> reports/"${UNMERGED_DIR}.log"
+			echo "Aligning ${f} and ${f/R1_001_val_1/R2_001_val_2} with BWA MEM" 
+
+			OUTNAME=$(basename ${f})
 
 			bwa mem -M -t 8 "$REF" "$f" "${f/R1_001_val_1/R2_001_val_2}" | \
-			    samtools view -Shb - > bam/"${f/R1_001_val_1.fq.gz/unsorted.bam}"
+			    samtools view -Shb - > bam/"${OUTNAME/R1_001_val_1.fq.gz/unsorted.bam}"
 
 	done
 
@@ -114,49 +116,54 @@ for f in fastq/*R1_001_val_1.fq.gz
 # PICARD TOOLS
 for f in bam/*unsorted.bam
 	do
-
-			echo "Cleaning, Running PICARD Tools, and sorting ${f} " >> reports/"${UNMERGED_DIR}.log"
-
+			echo "Cleaning and sorting ${f} " 
+			BASE=$(basename ${f})
+			
 			# clean unsorted bamfile:
-			java -jar $picard_path CleanSam \
-			    INPUT=bam/"$f" \
-			    OUTPUT=bam/"${f/unsorted.bam/unsorted.cleaned.bam}"
-
-      # sort bam
-      samtools sort -o bam/"${f/unsorted/sorted}" -O bam -T temp -@ 8 bam/"${f/unsorted.bam/unsorted.cleaned.bam}"
-
-      # removing the add or replace read groups steps here
-#			java -jar ~/biotools/picard/build/libs/picard.jar AddOrReplaceReadGroups VALIDATION_STRINGENCY=LENIENT \
-#					I="$f" O="${f/sorted/RG}" RGID="${f/_sorted.bam/}" RGLB="${a/_sorted.bam/}" RGPL=ILLUMINA \
-#					RGPU="${f/_sorted.bam/}" RGSM="${f/_sorted.bam/}" USE_JDK_DEFLATER=true USE_JDK_INFLATER=true
-
-			java -jar $picard_path MarkDuplicates \
-          VALIDATION_STRINGENCY=STRICT \
-					INPUT=bam/"${f/unsorted/sorted}" \
-					OUTPUT=bam/"${f/unsorted/strict.dedup}" \
-					METRICS_FILE=reports/"${f/unsorted.bam/strict.dedup.metrics.txt}" \
-			    ASSUME_SORTED=true
-
-			java -jar ~/biotools/picard/build/libs/picard.jar MarkDuplicates \
+			picard CleanSam \
+					INPUT="$f" \
+					OUTPUT="${f/unsorted.bam/unsorted.cleaned.bam}"
+	
+			# sort bam 
+			samtools sort -o "${f/unsorted/sorted}" -O bam -T temp -@ 8 "${f/unsorted.bam/unsorted.cleaned.bam}"
+		   
+			echo "Running MarkDuplicates ${f}" 
+			picard MarkDuplicates \
+					VALIDATION_STRINGENCY=STRICT \
+					INPUT="${f/unsorted/sorted}" \
+					OUTPUT="${f/unsorted/strict.dedup}" \
+					METRICS_FILE=reports/"${BASE/unsorted.bam/strict.dedup.metrics.txt}" \
+					ASSUME_SORTED=true
+	
+	    	  # removing the add or replace read groups steps here
+	#				java -jar ~/biotools/picard/build/libs/picard.jar AddOrReplaceReadGroups VALIDATION_STRINGENCY=LENIENT \
+	#						I="$f" O="${f/sorted/RG}" RGID="${f/_sorted.bam/}" RGLB="${a/_sorted.bam/}" RGPL=ILLUMINA \
+	#						RGPU="${f/_sorted.bam/}" RGSM="${f/_sorted.bam/}" USE_JDK_DEFLATER=true USE_JDK_INFLATER=true
+	
+					#java -jar $picard_path MarkDuplicates \
+					#java -jar ~/biotools/picard/build/libs/picard.jar MarkDuplicates \
+	
+			picard MarkDuplicates \ 
 					VALIDATION_STRINGENCY=LENIENT \
-					INPUT=bam/"${f/unsorted/sorted}" \
+					INPUT="${f/unsorted/sorted}" \
 					OUTPUT="${f/unsorted/lenient.dedup}" \
-					METRICS_FILE=reports/"${f/sorted.bam/lenient.dedup.metrics.txt}" \
-          ASSUME_SORTED=true
-#					REMOVE_DUPLICATES=TRUE \
-#					USE_JDK_DEFLATER=true \
-#					USE_JDK_INFLATER=true
+					METRICS_FILE=reports/"${BASE/sorted.bam/lenient.dedup.metrics.txt}" \
+					ASSUME_SORTED=true
+	#				REMOVE_DUPLICATES=TRUE \
+	#				USE_JDK_DEFLATER=true \
+	#				USE_JDK_INFLATER=true
 
+			echo "Indexing ${f}" 
+			samtools index "$f"
+	
 	done
 
 
-# INDEX BAMS
+# EVAL BAMS
 for f in bam/*dedup.bam
 	do
-			echo "Indexing deduplicated bam: ${f}" >> reports/"${UNMERGED_DIR}.log"
-			samtools index "$f"
-			echo "$f" >> reports/samtools_stats.log
-			samtools flagstat "$f" >> reports/samtools_stats.log
+			echo "BAM  ${f}" >> reports/samtools_stats.log
+			samtools flagstat $a >> reports/samtools_stats.log
 			echo %%%%%%%%%%%%%%% >> reports/samtools_stats.log
 			echo  >> reports/samtools_stats.log
 	done
@@ -165,29 +172,35 @@ for f in bam/*dedup.bam
 # GATK ANALYSIS
 for f in bam/*dedup.bam
 	do
-	echo "Running GATK on ${f}" >> reports/"${UNMERGED_DIR}.log"
-
-	java -jar ~/biotools/gatk-3.5/GenomeAnalysisTK.jar -T HaplotypeCaller \
-	  -R "$REF" -I "$f" \
-	  --sample_ploidy 1 \
-	  -O gatk/"${f/dedup.bam/gatk.haploid.vcf}"
-
-  java -jar $gatk_path HaplotypeCaller \
-    -R "$REF" -I "$f" \
-    -O gatk/"${f/dedup.bam/gatk.diploid.vcf}"
+			echo "Running GATK on ${f}" 
+			BASE=$(basename ${f})
+			
+			~/biotools/gatk-4.2.0.0/gatk HaplotypeCaller \
+					-R "$REF" -I "$f" \
+				   	--sample_ploidy 1 \
+				   	-O gatk/"${BASE/dedup.bam/gatk.haploid.vcf}"
+		   
+			~/biotools/gatk-4.2.0.0/gatk HaplotypeCaller \
+				   	-R "$REF" -I "$f" \
+				   	-O gatk/"${BASE/dedup.bam/gatk.diploid.vcf}"
 
 	done
 
 
 # FREEBAYES
+
 for f in bam/*.dedup.bam
 do
-  echo "Running freebayes on ${f}" >> reports/"${UNMERGED_DIR}.log"
-  freebayes -f "$REF" --ploidy 1 bam/"$f" > freebayes/"${f/dedup.bam/freebayes.vcf}"
+	   	echo "Running freebayes on ${f}" 
+	   	BASE=$(basename ${f})
 
+	   	~/biotools/freebayes-1.3.4-linux-static-AMD64 -f "$REF" --ploidy 1 "$f" > freebayes/"${BASE/dedup.bam/freebayes.vcf}"
+done
 
 
 ## SNIPPY
 #  $snippy_path --cpus $num_procs --outdir $out_dir/$root-snippy --prefix $root --cleanup --ref $ref --R1 $fq_1 --R2 $fq_2
 #  print SH "mv $out_dir/$root-snippy/$root.filt.vcf $out_dir/$root.$aligner.$caller.vcf
 #  rm -r $out_dir/$root-snippy
+
+
